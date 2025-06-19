88a3274be2b4dd9b61fa65edceee6f5bbc47a34e
nam <hoangnam1342005@.com>
2025-06-19 16:24:35 +0700

    create project

---
+++ mavenproject3/src/main/java/com/mycompany/mavenproject3/PrimaryController.java
@@ -0,0 +1,16 @@
+package com.mycompany.mavenproject3;
+
+import com.nhn.utils.MyAlert;
+import javafx.event.ActionEvent;
+
+
+
+public class PrimaryController {
+
+    public void handleQuestion(ActionEvent event){
+        MyAlert.getInstance().showMes("Coming soon...");
+    }
+    public void handleSignUP(ActionEvent event){
+        MyAlert.getInstance().showMes("Coming soon...");
+    }
+}
